d90d4faf0aa222278ec252390634dd052e8b1199
Djigurda <KoliaDukalis85@mail.ru>
2025-04-20 00:34:46 +0300

    initial commit added Dockerfile and Docker-compose.yaml

--- src/routing/router.py
+++ src/routing/router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+
 
 from src.services.base_services import main_service
 
 router=APIRouter()
 
 @router.get('/')
-def main():
-    return main_service()
+def main(response=Depends(main_service)):
+    return response
